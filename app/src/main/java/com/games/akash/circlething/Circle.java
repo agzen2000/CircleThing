1f7a975ff2288b4a2e4e397c8af176c70341eed3
agzen2000 <akashsen.as00@gmail.com>
2017-05-18 10:08:01 -0700

    Made the Ring

--- app/src/main/java/com/games/akash/circlething/Circle.java
+++ app/src/main/java/com/games/akash/circlething/Circle.java
@@ -3,9 +3,8 @@ package com.games.akash.circlething;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
 import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.ArcShape;
 import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
 import android.view.View;
@@ -16,7 +15,10 @@ import android.view.View;
 
 public class Circle extends View
 {
-    private ShapeDrawable mDrawable;
+    private ShapeDrawable baseCircle;
+    private ShapeDrawable goodGate;
+    private ShapeDrawable badGate;
+    private ShapeDrawable topCircle;
     int mWidth= this.getResources().getDisplayMetrics().widthPixels;
     int mHeight= this.getResources().getDisplayMetrics().heightPixels;
 
@@ -38,18 +40,34 @@ public class Circle extends View
 
     public void init(Context context)
     {
+        int radius = 2 * mWidth/5;
         int x = mWidth/2;
         int y = mHeight/2;
-        int radius = mWidth/3;
 
-        mDrawable = new ShapeDrawable(new OvalShape());
-        mDrawable.getPaint().setColor(Color.BLACK);
-        mDrawable.setBounds(x-radius, y-radius, x+radius, y+radius);
+
+        baseCircle = new ShapeDrawable(new OvalShape());
+        baseCircle.getPaint().setColor(Color.GRAY);
+        baseCircle.setBounds(x-radius, y-(int)(1.5*radius), x+radius, y+(int)(0.5*radius));
+
+        goodGate = new ShapeDrawable(new ArcShape(260,20));
+        goodGate.getPaint().setColor(Color.GREEN);
+        goodGate.setBounds(x-radius, y-(int)(1.5*radius), x+radius, y+(int)(0.5*radius));
+
+        badGate= new ShapeDrawable(new ArcShape(80,20));
+        badGate.getPaint().setColor(Color.RED);
+        badGate.setBounds(x-radius, y-(int)(1.5*radius), x+radius, y+(int)(0.5*radius));
+
+        topCircle = new ShapeDrawable(new OvalShape());
+        topCircle.getPaint().setColor(Color.WHITE);
+        topCircle.setBounds(x-radius+x/8, y-(int)(1.5*radius)+x/8, x+radius - x/8, y+(int)(0.5*radius) - x/8);
     }
 
     @Override
     protected void onDraw(Canvas canvas)
     {
-        mDrawable.draw(canvas);
+        baseCircle.draw(canvas);
+        goodGate.draw(canvas);
+        badGate.draw(canvas);
+        topCircle.draw(canvas);
     }
 }
